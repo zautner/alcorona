ba204781d0cd9e22938f315f80cb5d612e12886f
zautner <zautner@gmail.com>
2020-04-11 16:13:16 +0300

    [*] HTML template with bootstrap 4

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-echarts/go-echarts/templates"
+	"github.com/pingcap/log"
+	"io"
 	"io/ioutil"
 	"math/rand"
+	"mime"
 	"os"
 	"sort"
 	"strconv"
@@ -13,8 +17,39 @@ import (
 	"html/template"
 	"net/http"
 
-	"github.com/chenjiandongx/go-echarts/charts"
+	"github.com/go-echarts/go-echarts/charts"
 )
+
+const pageTemplate = `{{- define "page" }}
+	{{- template "header" . }}
+	<div>
+	{{- template "routers" . }}
+	{{- range .Charts }}
+	{{ template "base" . }}
+	<br/>
+	{{- end }}
+	<style>
+	.container {display: flex;justify-content: center;align-items: center; width:"96%"}
+	.item {margin: auto;}
+	</style>
+	</div>
+	{{ end }}`
+const chartTemplate = `{{- define "chart" }}
+<div class=container-fluid>
+{{- range .JSAssets.Values }}
+    <script src="{{ . }}"></script>
+{{- end }}
+{{- range .CSSAssets.Values }}
+    <link href="{{ . }}" rel="stylesheet">
+{{- end }}
+{{- template "routers" . }}
+{{- template "base" . }}
+<style>
+    .item {margin: auto;}
+</style>
+</div>
+{{ end }}
+`
 
 var (
 	HEADER_ONE       = os.Getenv("header_rapidapi")
@@ -24,6 +59,10 @@ var (
 )
 
 //////// TYPES /////////
+type Countries struct {
+	AffectedCountries []string `json:"affected_countries"`
+}
+
 /*CoronaRecord holds one report per country */
 type CoronaRecord struct {
 	IDString                   string `json:"id,unknown"`
@@ -47,6 +86,8 @@ type CoronaList struct {
 	URL            []string
 	sb             string
 	asc            bool
+	Countries      []string
+	Charts         template.HTML
 }
 
 func (d *CoronaList) Len() int { return len(d.StatsByCountry) }
@@ -60,7 +101,7 @@ func (d *CoronaList) Less(i, j int) bool {
 }
 
 func (d *CoronaList) timeSeries() []string {
-	//ret := make([]time.Time, len(d.StatsByCountry))
+	//ret := make([]time.Time, len(Countries.StatsByCountry))
 	ret := make([]string, len(d.StatsByCountry))
 	for i, val := range d.StatsByCountry {
 		t := strings.Split(val.RecordDate, " ")[0]
@@ -96,44 +137,11 @@ func (d *CoronaList) newCases() []int {
 	return ret
 }
 
-func (d *CoronaList) criticalRelative() []float32 {
-	ret := make([]float32, len(d.StatsByCountry))
-	for i, val := range d.StatsByCountry {
-		var t float32
-		if i < 7 {
-			ret[i] = 0.0
-			println(t, ret[i])
-		} else {
-			var denominator float32
-			_, e := fmt.Sscan(strings.ReplaceAll(val.SeriousCriticalString, ",", ""), &t)
-			_, e = fmt.Sscan(strings.ReplaceAll(val.ActiveCasesString, ",", ""), &denominator)
-			if e == nil {
-				ret[i] = t / denominator
-			}
-		}
-		println(t, ret[i])
-	}
-	return ret
-}
-
-unc (d *CoronaList) critical() []float32 {
-	ret := make([]float32, len(d.StatsByCountry))
-	for i, val := range d.StatsByCountry {
-		var t float32
-			_, e := fmt.Sscan(strings.ReplaceAll(val.SeriousCriticalString, ",", ""), &t)
-			if e == nil {
-				ret[i] = t
-			}
-		}
-		println(t, ret[i])
-	}
-	return ret
-}
 func (d *CoronaList) newDeaths() []int {
 	ret := make([]int, len(d.StatsByCountry))
 	for i, val := range d.StatsByCountry {
 		var t int
-		_, e := fmt.Sscan(val.NewDeathsString, &t)
+		_, e := fmt.Sscan(strings.ReplaceAll(val.NewDeathsString, ",", ""), &t)
 		if e == nil {
 			ret[i] = t
 		}
@@ -145,7 +153,7 @@ func (d *CoronaList) deaths() []int {
 	ret := make([]int, len(d.StatsByCountry))
 	for i, val := range d.StatsByCountry {
 		var t int
-		_, e := fmt.Sscan(val.TotalDeathsString, &t)
+		_, e := fmt.Sscan(strings.ReplaceAll(val.TotalDeathsString, ",", ""), &t)
 		if e == nil {
 			ret[i] = t
 		}
@@ -156,28 +164,87 @@ func (d *CoronaList) deaths() []int {
 
 //////// VARIABLES /////////
 var tpl *template.Template
+var countries = Countries{
+	AffectedCountries: []string{},
+}
 
 //////// FUNCTIONS /////////
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	templates.PageTpl = pageTemplate
+	templates.ChartTpl = chartTemplate
+	tpl = template.Must(template.ParseGlob("templates/*.go*html"))
+	countriesInit()
+}
+
+func countriesInit() {
+	url := "https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php"
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add(HEADER_ONE, "coronavirus-monitor.p.rapidapi.com")
+	req.Header.Add(HEADER_TWO, HEADER_TWO_VALUE)
+
+	res, err := http.DefaultClient.Do(req)
+	if nil != err {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	log.Error(string(body)[:1])
+
+	json.Unmarshal(body, &countries)
+	sort.Strings(countries.AffectedCountries)
 }
 
 func main() {
+	http.HandleFunc("/download/", download)
+	http.HandleFunc("/templates/", load)
+	http.HandleFunc("/public/", load)
+	http.HandleFunc("/assets/", load)
 	http.HandleFunc("/chart/", chart)
 	http.HandleFunc("/", index)
-	println(1)
 	err := http.ListenAndServe(":80", nil)
 	if nil != err {
-		println("Error!", err.Error())
+		log.Error(err.Error())
 	}
 }
 
+func download(writer http.ResponseWriter, request *http.Request) {
+	fns := strings.Split(filename(request), "/")
+	println("file", fns[len(fns)-1])
+	d, _ := readData(fns[len(fns)-1], false)
+	b, e := json.Marshal(d.StatsByCountry)
+	if e != nil {
+		http.Error(writer, e.Error(), http.StatusForbidden)
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	ct := http.DetectContentType(b)
+	ebt, e := mime.ExtensionsByType(ct)
+	if e != nil {
+		http.Error(writer, e.Error(), http.StatusForbidden)
+	}
+	writer.Header().Set("Content-Disposition", "attachment; filename="+d.Country+ebt[0])
+	writer.Write(b)
+}
+
+func load(writer http.ResponseWriter, request *http.Request) {
+	bs, _ := ioutil.ReadFile(filename(request))
+	writer.Write(bs)
+}
+
+func filename(request *http.Request) string {
+	return fmt.Sprintf("./%s", request.RequestURI)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	println(HEADER_ONE, HEADER_ONE_VALUE, HEADER_TWO, HEADER_TWO_VALUE)
 	d := getDataJson(r.RequestURI[1:])
-	tpl.ExecuteTemplate(w, "index.gohtml", d)
-	drawChart(d, w)
+	sw := &strings.Builder{}
+	drawChart(d, sw)
+	d.Charts = template.HTML(sw.String())
+	e := tpl.Lookup("index.gohtml").Execute(w, (d))
+	if e != nil {
+		log.Error(e.Error())
+	}
 }
 
 func chart(w http.ResponseWriter, r *http.Request) {
@@ -185,36 +252,52 @@ func chart(w http.ResponseWriter, r *http.Request) {
 	drawChart(d, w)
 }
 
-func drawChart(d *CoronaList, w http.ResponseWriter) {
-	graph := charts.NewLine()
-	graph.SetGlobalOptions(charts.TitleOpts{Left: "10", Right: "10", Title: "Corona cases", Subtitle: d.Country},
-		charts.LegendOpts{Left: "10", Right: "10"},
+func drawChart(d *CoronaList, sw io.Writer) {
+	graphTotal := charts.NewLine()
+	graphTotal.SetGlobalOptions(charts.TitleOpts{Title: "COVID-19 cases", Subtitle: d.Country},
+		charts.LegendOpts{Left: "200px", Top: "5px", TextStyle: charts.TextStyleOpts{FontSize: 12}},
 		charts.TooltipOpts{Show: true},
 		charts.YAxisOpts{Scale: true, Type: "value"},
 	)
+	graphTotal.AddXAxis(d.timeSeries()).AddYAxis("Total cases", d.totalCases())
+	grapTotalDeaths := charts.NewBar()
+	grapTotalDeaths.SetGlobalOptions(charts.TitleOpts{Title: "COVID-19 deaths", Subtitle: d.Country},
+		charts.LegendOpts{Left: "200px", Top: "5px", TextStyle: charts.TextStyleOpts{FontSize: 12}},
+		charts.TooltipOpts{Show: true},
+		charts.YAxisOpts{Scale: true, Type: "value"},
+	)
+	grapTotalDeaths.AddXAxis(d.timeSeries()).AddYAxis("Deaths", d.deaths())
 
-	graph.AddXAxis(d.timeSeries()).AddYAxis("Total cases", d.totalCases()).AddYAxis("Deaths", d.deaths()).AddYAxis("New cases", d.critical())
-	graphES := charts.NewEffectScatter()
-	graphES.SetGlobalOptions(
-		charts.LegendOpts{Left: "100", Right: "10"},
+	graphNewCases := charts.NewEffectScatter()
+	graphNewCases.SetGlobalOptions(charts.TitleOpts{Title: "New cases", Subtitle: d.Country},
+		charts.LegendOpts{Left: "200px", Top: "5px", TextStyle: charts.TextStyleOpts{FontSize: 12}},
 		charts.TooltipOpts{Show: true},
 		charts.YAxisOpts{Scale: true, Type: "value"},
-		charts.ColorOpts{"Red", "black", "orange", "brown", "navy", "peach"},
 	)
-	graphES.AddXAxis(d.timeSeries()).
-		AddYAxis("New", d.newCases(), charts.RippleEffectOpts{Period: 5, Scale: 6, BrushType: "line"}).
-		AddYAxis("Critical rel", d.criticalRelative(), charts.RippleEffectOpts{Period: 10, Scale: 6, BrushType: "line"}).
-		AddYAxis("New deaths", d.newDeaths(), charts.RippleEffectOpts{Period: 2, Scale: 10, BrushType: "stroke"})
+	graphNewCases.AddXAxis(d.timeSeries()).
+		AddYAxis("New cases", d.newCases(), charts.RippleEffectOpts{Period: 3, Scale: 6, BrushType: "fill"})
+
+	graphNewD := charts.NewEffectScatter()
+	graphNewD.SetGlobalOptions(charts.TitleOpts{Title: "New deaths", Subtitle: d.Country},
+		charts.LegendOpts{Left: "200px", Top: "5px", TextStyle: charts.TextStyleOpts{FontSize: 12}},
+		charts.TooltipOpts{Show: true},
+		charts.YAxisOpts{Scale: true, Type: "value"},
+	)
+	graphNewD.AddXAxis(d.timeSeries()).
+		AddYAxis("New deaths", d.newDeaths(), charts.RippleEffectOpts{Period: 4, Scale: 10, BrushType: "stroke"})
 	f, e := os.Create("line-" + strconv.Itoa(rand.Int()) + ".html")
-	defer os.Remove(f.Name())
 	if e == nil {
-		graphES.Overlap(graph)
-		graphES.Render(w, f)
+		defer os.Remove(f.Name())
+		graphTotal.Render(sw, f)
+		grapTotalDeaths.Render(sw, f)
+		graphNewCases.Render(sw, f)
+		graphNewD.Render(sw, f)
+	} else {
 	}
 }
 
 func getDataJson(uri string) *CoronaList {
-	println(uri)
+	log.Error(uri)
 	country := "Israel"
 	splitten := strings.Split(uri, "/")
 	if len(splitten) > 0 {
@@ -241,8 +324,8 @@ func readData(country string, asc bool) (*CoronaList, error) {
 	}
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	println(string(body)[:1])
-	d := CoronaList{URL: []string{country}, asc: asc}
+	log.Error(string(body)[:1])
+	d := CoronaList{URL: []string{country}, asc: asc, Countries: countries.AffectedCountries}
 	json.Unmarshal(body, &d)
 	statsByCountry := make([]CoronaRecord, 0)
 	sort.Sort(&d)
